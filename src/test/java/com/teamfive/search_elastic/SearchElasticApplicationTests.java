c3012bbc6de63fc279dd1b9186e8e340eec84a1f
jayjoshi <jay.joshi@coviam.com>
2019-01-25 12:26:50 +0530

    final version v1

---
+++ src/test/java/com/teamfive/search_elastic/SearchElasticApplicationTests.java
@@ -0,0 +1,17 @@
+package com.teamfive.search_elastic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchElasticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
